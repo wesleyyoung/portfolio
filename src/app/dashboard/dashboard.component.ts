da167d04865607bc60a71d69962d5f0b91d9e0b4
wesleyyoung <wesley.young.portfolio@gmail.com>
2019-11-19 20:18:59 -0700

    fixed 'Goodbye' font-size for mobile

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -62,12 +62,16 @@ export class DashboardComponent implements OnInit {
   public titleChange;
 
   @ViewChild('molecularBg', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  //@ViewChild('sunrise', { static: true }) sunriseCanvas: ElementRef<HTMLCanvasElement>;
+  //@ViewChild('starfield', { static: true }) starCanvas: ElementRef<HTMLCanvasElement>;
 
   public intro: Boolean = false;
   public introBtn: Boolean = false;
   public introBtnTxt: string = '';
 
   private ctx: CanvasRenderingContext2D;
+  private sctx: CanvasRenderingContext2D;
+  private star_ctx: CanvasRenderingContext2D;
 
   public squigglePoints: Array<string> = [
     '0,0',
@@ -103,6 +107,12 @@ export class DashboardComponent implements OnInit {
 
       this.canvas.nativeElement.setAttribute('height', window.innerHeight + 'px');
       this.canvas.nativeElement.setAttribute('width', window.innerWidth + 'px');
+
+      //this.sunriseCanvas.nativeElement.setAttribute('height', (window.innerHeight * 2) + 'px');
+      //this.sunriseCanvas.nativeElement.setAttribute('width', window.innerWidth + 'px');
+
+      //this.starCanvas.nativeElement.setAttribute('height', (window.innerHeight) + 'px');
+      //this.starCanvas.nativeElement.setAttribute('width', window.innerWidth + 'px');
     });
   }
 
@@ -131,15 +141,51 @@ export class DashboardComponent implements OnInit {
     runtime.init();
   }
 
+  public draw_sunrise(): void {
+
+    const sunrise: Sunrise = new Sunrise(this.sctx);
+
+    const runtime: RunTime = new RunTime(this.sctx, [
+      sunrise
+    ], this.ngZone);
+
+    runtime.init();
+  }
+
+  public draw_starfield(): void {
+
+    const starfield: Starfield = new Starfield(this.star_ctx);
+
+    const runtime: RunTime = new RunTime(this.star_ctx, [
+      starfield
+    ], this.ngZone);
+
+    runtime.init();
+  }
+
   ngOnInit() {
 
     this.canvas.nativeElement.setAttribute('height', window.innerHeight + 'px');
     this.canvas.nativeElement.setAttribute('width', window.innerWidth + 'px');
 
+    //this.sunriseCanvas.nativeElement.setAttribute('height', (window.innerHeight * 2) + 'px');
+    //this.sunriseCanvas.nativeElement.setAttribute('width', window.innerWidth + 'px');
+
+    //this.starCanvas.nativeElement.setAttribute('height', (window.innerHeight) + 'px');
+    //this.starCanvas.nativeElement.setAttribute('width', window.innerWidth + 'px');
+
     this.ctx = this.canvas.nativeElement.getContext('2d');
+    //this.sctx = this.sunriseCanvas.nativeElement.getContext('2d');
+    //this.star_ctx = this.starCanvas.nativeElement.getContext('2d');
 
     this.ctx.canvas.width = window.innerWidth;
     this.ctx.canvas.height = window.innerHeight;
+
+    //this.sctx.canvas.width = window.innerWidth;
+    //this.sctx.canvas.height = (window.innerHeight * 2);
+
+    //this.star_ctx.canvas.width = window.innerWidth;
+    //this.star_ctx.canvas.height = (window.innerHeight);
 
     this.api.isMobile.subscribe(isMobile => {
       this.isMobile = isMobile;
@@ -159,7 +205,9 @@ export class DashboardComponent implements OnInit {
 
     this.draw_skill_2();
 
-    
+    // this.draw_sunrise();
+
+    // this.draw_starfield();
 
     this.intro = true;
 
@@ -171,6 +219,156 @@ export class DashboardComponent implements OnInit {
     }, 1500);
   }
 
+}
+
+export interface Star {
+  x: number;
+  y: number;
+  speed: number;
+}
+
+export class Starfield {
+
+  private starCount: number = 40;
+  private stars: Star[] = [];
+
+  constructor(
+    private ctx: CanvasRenderingContext2D
+  ) {
+    let modifiers = [0.25, 0.5, 0.75, 1, 2];
+
+    for (var i = 0; i < this.starCount; i++) {
+      this.stars.push({
+        x: Math.floor((Math.random() * this.ctx.canvas.width) + 1),
+        y: Math.floor((Math.random() * this.ctx.canvas.height) + 1),
+        speed: modifiers[Math.floor((Math.random() * modifiers.length - 1))],
+      });
+    }
+  }
+
+  update() {
+
+    for (var i = 0; i < this.starCount; i++) {
+
+      this.stars[i].x += this.stars[i].speed;
+      this.stars[i].y -= this.stars[i].speed;
+
+      if (this.stars[i].x >= this.ctx.canvas.width) {
+        this.stars[i].x = 0;
+      } else if (this.stars[i].x <= 0) {
+        this.stars[i].x = this.ctx.canvas.width;
+      }
+
+      if (this.stars[i].y >= this.ctx.canvas.height) {
+        this.stars[i].x = 0;
+      } else if (this.stars[i].y <= 0) {
+        this.stars[i].y = this.ctx.canvas.height;
+      }
+    }
+  }
+
+  draw() {
+
+    this.update();
+
+    this.ctx.fillStyle = "white";
+
+    for (var i = 0; i < this.starCount; i++) {
+      this.ctx.fillRect(this.stars[i].x, this.stars[i].y, 2, 2);
+    }
+
+  }
+}
+
+export class Sunrise {
+
+  private sunx1: number = 0;
+  private suny1: number = 0;
+  private sunx2: number = this.ctx.canvas.width;
+  private suny2: number = this.ctx.canvas.height;
+
+  private sunx1Mod: number = 5;
+  private suny1Mod: number = 5;
+  private sunx2Mod: number = -5;
+  private suny2Mod: number = -5;
+
+  private m11: number = 1;
+  private m11Mod: number = .001;
+
+  constructor(
+    private ctx: CanvasRenderingContext2D
+  ) {
+  }
+
+  update() {
+
+    this.m11 += this.m11Mod;
+
+    this.ctx.transform(this.m11, 0, 0, this.m11, 0, 0);
+
+    if (this.m11 >= 1.04 || this.m11 <= 1) {
+      this.m11Mod *= -1;
+    }
+
+    this.sunx1 += this.sunx1Mod;
+    this.suny1 += this.suny1Mod;
+    this.sunx2 += this.sunx2Mod;
+    this.suny2 += this.suny2Mod;
+
+    if (this.sunx1 >= this.ctx.canvas.width) {
+      this.sunx1Mod *= -1;
+      this.sunx1 = this.ctx.canvas.width;
+    } else if (this.sunx1 <= 0) {
+      this.sunx1Mod *= -1;
+      this.sunx1 = 0;
+    }
+
+    if (this.suny1 >= (this.ctx.canvas.height / 4)) {
+      this.suny1Mod *= -1;
+    } else if (this.suny1 <= 0) {
+      this.suny1Mod *= -1;
+      this.suny1 = 0;
+    }
+
+    if (this.sunx2 >= this.ctx.canvas.width) {
+      this.sunx2 = this.ctx.canvas.width;
+      this.sunx2Mod *= -1;
+    } else if (this.sunx2 <= 0) {
+      this.sunx2Mod *= -1;
+      this.sunx2 = 0;
+    }
+
+    if (this.suny2 <= this.ctx.canvas.height - (this.ctx.canvas.height / 3)) {
+      this.suny2Mod *= -1;
+      this.suny2 = this.ctx.canvas.height - (this.ctx.canvas.height / 3);
+    } else if (this.suny2 >= this.ctx.canvas.height) {
+      this.suny2Mod *= -1;
+      this.suny2 = this.ctx.canvas.height;
+    }
+  }
+
+  draw() {
+
+    this.update();
+
+    let sunGrad = this.ctx.createLinearGradient(this.sunx1, this.suny1, this.sunx2, this.suny2);
+
+    sunGrad.addColorStop(0, "#584053");
+    sunGrad.addColorStop(0.35, "#F97B4F");
+    sunGrad.addColorStop(1, "#FCBC66");
+
+    this.ctx.fillStyle = sunGrad;
+
+    this.ctx.filter = "blur (10px)";
+
+    this.ctx.beginPath();
+
+    this.ctx.arc(this.ctx.canvas.width / 2, this.ctx.canvas.height / 2, 150, 0, 2 * Math.PI);
+
+    this.ctx.fill();
+
+    this.ctx.resetTransform();
+  }
 }
 
 export interface Molecule {
